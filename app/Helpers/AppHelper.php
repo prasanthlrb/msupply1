89a61bfe9eabc95e8f6126562c88a9fc2c6a3971
lrbtech <info@lrbtech.com>
2019-11-23 11:55:15 +0530

    unhide sms

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -7,28 +7,28 @@ class AppHelper
 
     public function sendMessage($msg, $phone)
     {
-        // $requestParams = array(
-        //     'route' => '1',
-        //     'username' => '8870050001',
-        //     'password' => 'welcome*85',
-        //     'senderid' => 'MSUPLY',
-        //     'number' => $phone,
-        //     'message' => $msg
-        // );
-        // //merge API url and parameters
-        // $apiUrl = 'http://api.onhandsms.com/api/v2/sendsms?';
-        // foreach ($requestParams as $key => $val) {
-        //     $apiUrl .= $key . '=' . urlencode($val) . '&';
-        // }
-        // $apiUrl = rtrim($apiUrl, "&");
+        $requestParams = array(
+            'route' => '1',
+            'username' => '8870050001',
+            'password' => 'welcome*85',
+            'senderid' => 'MSUPLY',
+            'number' => $phone,
+            'message' => $msg
+        );
+        //merge API url and parameters
+        $apiUrl = 'http://api.onhandsms.com/api/v2/sendsms?';
+        foreach ($requestParams as $key => $val) {
+            $apiUrl .= $key . '=' . urlencode($val) . '&';
+        }
+        $apiUrl = rtrim($apiUrl, "&");
 
-        // //API call
-        // $ch = curl_init();
-        // curl_setopt($ch, CURLOPT_URL, $apiUrl);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        //API call
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $apiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        // curl_exec($ch);
-        // curl_close($ch);
+        curl_exec($ch);
+        curl_close($ch);
 
         return true;
     }
